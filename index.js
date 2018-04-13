0b22be6bedd82f484c2caedc522edd15dc0aa00d
Tamás Németh <tomuwhu@gmail.com>
2018-04-13 16:27:51 +0200

    .

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-var inp    = require ( 'prompt-sync' )()
-var fss    = require ( 'fs' )
-var fin    = inp     ( 'Fájlnév: ' )
-console    . log     ( fin )
-var str    = fss
-           . readFileSync
-                     ( fin,'UTF-8' )
-console    . log     (str)
-var arr    = str
-           . split   ( '\n' )
-    arr    . sort    ( )
-var out    = ""
-    arr    . forEach ( v => out += v + "\n")
-console    . log     ( out )
-    fss    . writeFileSync
-                     ( 'out.txt', out )
+var bekér  =  require ( 'prompt-sync' )()
+kiír = (x) => console.log(x)
+
+//JS kód:
+var x = bekér('írj be egy számot: ')
+kiír(`A kétszerese: ${ 2 * x }`)
