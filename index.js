61a27a1a58476c9c3a0c5e240954afccc63e717b
Tamás Németh <tomuwhu@gmail.com>
2018-04-13 16:58:16 +0200

    .

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var inp    = require ( 'prompt-sync' )()
-var fss    = require ( 'fs' )
-var fin    = inp     ( 'Fájlnév: ' )
-console    . log     ( fin )
-var str    = fss
-           . readFileSync
-                     ( fin,'UTF-8' )
-console    . log     (str)
-var arr    = str
-           . split   ( '\n' )
-    arr    . sort    ( )
-var out    = ""
-    arr    . forEach ( v => out += v + "\n")
-console    . log     ( out )
-    fss    . writeFileSync
-                     ( 'out.txt', out )
+var inp = require ( 'prompt-sync' )()
+var fss = require ( 'fs' )
+var fin = inp     ( 'Fájlnév: ' )
+console . log     ( fin )
+var str = fss
+        . readFileSync
+                  ( fin,'UTF-8' )
+console . log     (str)
+var arr = str
+        . split   ( '\n' )
+    arr . sort    ( )
+var out = ""
+    arr . forEach ( v => out += v + "\n")
+console . log     ( out )
+    fss . writeFileSync
+                  ( 'out.txt', out )
