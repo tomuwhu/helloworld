6b19b209835ea64fef3ae53caf969358db774c41
Tamás Németh <tomuwhu@gmail.com>
2018-04-18 12:54:19 +0200

    .

--- Morze.js
+++ Morze.js
@@ -1,6 +1,9 @@
 //1. feladat
 cc = require('exam-cc')
-
+var az = {
+    azt: ['a','e','i','o','u','ó','ő','ö','ű','ü','ú','í','é','á'],
+    az : (c='') => az.azt.includes(c.toLowerCase()) ? 'z' : ''
+}
 //6. feladat
 function Morze2Szöveg(s) {
         let answ = '', szavak = s.split(/ {7}/)
@@ -16,15 +19,15 @@ function Morze2Szöveg(s) {
 abc = cc.read('input/morzeabc.txt','\r\n','\t')
 
 //3. feladat
-cc.print( `3. feladat: ${abc.length}\n` )
+cc.print( `3. feladat: A morze abc ${abc.length} db karakter kódját tartalmazza\n` )
 
 //4. feladat
-var karakter = cc.input('karakter: ')
+var karakter = cc.input('4. feladat: Kérek egy karaktert: ')
 mc = cc.find(abc,'Betű',karakter)
 if (typeof mc=='undefined')
-  cc.print(`4. feladat: Nem található a kódtárban ilyen karakter!\n`)
+  cc.print(`            Nem található a kódtárban ilyen karakter!\n`)
 else
-  cc.print(`4. feladat: '${mc.Morzejel}'\n`)
+  cc.print(`            A${az.az(karakter)} ${karakter} karakter morze kódja: ${mc.Morzejel}\n`)
 
 //5. feladat
 text = cc.read('input/morze.txt','\r\n',';',0,0,['szerző','idézet'])
@@ -38,11 +41,11 @@ it = text.map( v => {
 cc.write('forditas.txt',it)
 
 //7. feladat
-cc.print('7. feladat:',it[0].szerző,'\n')
+cc.print('7. feladat: Az első idézet szerzője: ',it[0].szerző,'\n')
 
 //8. feladat
 it.sort( (a,b) => b.idézet.length-a.idézet.length)
-cc.print('8. feladat:',it[0].szerző+": "+it[0].idézet,'\n')
+cc.print('8. feladat: A leghosszabb idézet szerzője és az idézet:',it[0].szerző+": "+it[0].idézet,'\n')
 
 //9. feladat
 it_a = cc.filter( it , 'szerző', 'ARISZTOTELÉSZ' )
