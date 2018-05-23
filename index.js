3322b5feb1b9f5ad0409b9c4474a2bdcf7cc1ece
Tamás Németh <tomuwhu@gmail.com>
2018-05-23 19:23:44 +0200

    .

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-var input  =  require ( 'prompt-sync' )()
+const rl =
+  require('readline')
+    .createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+s = new Set()
+rl.on('line', line => {
+    if (line.length==0)
+        console.log(s),
+        rl.close()
+    else
+        line
+          .split(",")
+          .forEach( v => {
+              if ( s.has(v) ) console.log('Van már ilyen!')
+              else s.add(v), console.log('Beszúrva!')
+          } )
+})
+console.log(`
+Set demo.
 
-var x = input('Írj be egy számot: ')
-console.log(`A kétszerese: ${ 2 * x }`)
+Kilépés <Enter>!`
+);
