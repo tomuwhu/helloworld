3b6c746c3c08bf5f869f7267e11f236a992d00f7
Tamás Németh <tomuwhu@gmail.com>
2018-12-04 12:00:21 +0100

    .

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const cc = require('exam-cc')
 let file = cc.read('./files/kekszalag.csv')
-console.log(`1. feladat\nA beolvasásá és a tárolás kész!\n2. feladat\nÖsszesen ${ file.length } adatot tartalmaz az állomány`)
+console.log(`\n1. feladat\nA beolvasásá és a tárolás kész!\n\n2. feladat\nÖsszesen ${ file.length } adatot tartalmaz az állomány`)
 console.log(
-    `3. feladat\nAz elő 10 hajó adatai:`+
+    `\n3. feladat\nAz elő 10 hajó adatai:` +
     file.slice(0,10)
         .map( (v,k) => 
             `${k+1}. ${ v.hajó } -  ${ v.klub } - ${v.nap*60*24 + v.óra*60 + v.perc }`
         ).join('\n')
 )
-console.log(`4. feladat:\nA verseny kategóriái:`)
+console.log(`\n4. feladat:\nA verseny kategóriái:`)
 let kn=0
 file.toMap('kategória').forEach( (e,k) => {
     kn++
@@ -17,7 +17,7 @@ file.toMap('kategória').forEach( (e,k) => {
 console.log(`Összesen ${kn} kategória szerepel az adatok között!`)
 
 console.log(
-    `5.feladat\nAz első 3 hajó átlagsebessége\n`+
+    `\n5.feladat\nAz első 3 hajó átlagsebessége\n` +
     file.map( v => {
             v.as = 160 / (Number(v.nap) * 24 + Number(v.óra) + (Number(v.perc) / 60) )
             return v 
@@ -28,7 +28,7 @@ console.log(
         .join('\n')
 )
 console.log(
-    `6. feladat\nA leggyorsabb hajó ${
+    `\n6. feladat\nA leggyorsabb hajó ${
         ( Number(file[0].nap) * 24 * 60 + Number(file[0].óra * 60) + Number(file[0].perc) - 7*60 - 13  )
     } perccel maradt le az abszolút rekordtól.`
     )
@@ -37,4 +37,4 @@ cc.write('./files/hajonevek.txt', file.map(v => ({
     klub: v.klub,
     ido: `${v.nap}:${v.óra}:${v.perc}`
 })))
-console.log(`7. feladat\nA fájlba írás sikeresen megtörtént!`)
+console.log(`\n7. feladat\nA fájlba írás sikeresen megtörtént!`)
